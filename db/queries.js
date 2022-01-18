fb7b9ee0fe1b544480b55bab649b482771c3aced
Jake Pasterank <jdp.pasternak@gmail.com>
2022-01-17 18:12:35 -1000

    Add view all employees functionality

--- db/queries.js
+++ db/queries.js
@@ -22,4 +22,23 @@ const selectAllFromRole = () => {
     .catch((err) => err);
 };
 
-module.exports = { selectAllFromDepartment, selectAllFromRole };
+const selectAllFromEmployee = () => {
+  const sql = `
+    SELECT e.id, e.first_name, e.last_name, role.title, department.name AS department_name, role.salary, CONCAT(em.first_name," ", em.last_name) AS manager
+    FROM employee e
+    LEFT JOIN employee em ON e.manager_id = em.id
+    LEFT JOIN role ON e.role_id = role.id
+    LEFT JOIN department ON role.department_id = department.id
+    `;
+  return db
+    .promise()
+    .query(sql)
+    .then(([rows]) => rows)
+    .catch((err) => err);
+};
+
+module.exports = {
+  selectAllFromDepartment,
+  selectAllFromRole,
+  selectAllFromEmployee,
+};

362da1506d10997ff1e6d7c240153be08e716658
Jake Pasterank <jdp.pasternak@gmail.com>
2022-01-17 17:50:50 -1000

    Add functionality to view all roles

--- db/queries.js
+++ db/queries.js
@@ -9,4 +9,17 @@ const selectAllFromDepartment = () => {
     .catch((err) => console.log(err));
 };
 
-module.exports = { selectAllFromDepartment };
+const selectAllFromRole = () => {
+  const sql = `
+  SELECT role.title, role.id, department.name AS department_name, role.salary
+  FROM role
+  LEFT JOIN department ON role.department_id = department.id;
+  `;
+  return db
+    .promise()
+    .query(sql)
+    .then(([rows]) => rows)
+    .catch((err) => err);
+};
+
+module.exports = { selectAllFromDepartment, selectAllFromRole };
